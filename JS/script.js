5267833af152c58321d008870791aa2ffe9cd512
yakshu205 <yakshbutani1@gmail.com>
2026-08-02 13:24:15 +0530

    new changes

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,13 @@
 const slides = document.querySelectorAll(".slide");
 
-const next = document.querySelector(".next");
-
-const prev = document.querySelector(".prev");
-
 let index = 0;
 
-function showSlide(i){
-
+function showSlide(index) {
     slides.forEach(slide => slide.classList.remove("active"));
-
-    slides[i].classList.add("active");
-}
-
-next.onclick = () => {
-
-    index++;
-
-    if(index >= slides.length)
-        index = 0;
-
-    showSlide(index);
-}
-
-prev.onclick = () => {
-
-    index--;
-
-    if(index < 0)
-        index = slides.length - 1;
-
-    showSlide(index);
+    slides[index].classList.add("active");
 }
 
 setInterval(() => {
-
-    index++;
-
-    if(index >= slides.length)
-        index = 0;
-
+    index = (index + 1) % slides.length;
     showSlide(index);
-
-},3000);
+}, 3000);
